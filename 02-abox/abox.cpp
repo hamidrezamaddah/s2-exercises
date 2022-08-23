27d502340174096b1844113a4c69b254b2026bc3
hamidreza maddah <hamidrezamaddah@yahoo.com>
2022-08-23 09:52:29 +0430

    update abox project

--- 02-abox/abox.cpp
+++ 02-abox/abox.cpp
@@ -1,7 +1,73 @@
 #include <iostream>
+#include <string.h>
 
-int main(int argc, char const *argv[])
+bool is_number(char* str)
 {
-    /* code */
-    return 0;
+    for(int i = 0 ; i < strlen(str); i++)
+    {
+        if(!isdigit(str[i]))
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main(int argc, char* argv[])
+{
+ if(argc < 3 || !is_number(argv[1]) || !is_number(argv[2]))
+ {
+    std::cout << "Invalid arguments.";
+    return 1;
+ }
+ int h = atoi(argv[1]);
+ int w = atoi(argv[2]);
+ char draw = '*';
+ bool wire = false;
+ if(argc >= 4)
+ {
+    if(strstr(argv[3],"wire"))
+    {
+        wire = true;
+    }
+    else
+    {
+        draw = argv[3][0];
+    }
+ }
+
+ for(int i = 0 ; i < h ; i++)
+ {
+    for(int j = 0 ; j < w ; j++)
+    {
+        if(i != 0 && i != h-1 && j != 0 && j != w-1)
+        {
+            std::cout << " ";
+        }
+        else
+        {
+            if(wire)
+            {
+                if(( i == 0 && j == 0) || ( i == 0 && j == w-1) || ( i == h-1 && j == 0) || ( i == h-1 && j == w-1) )
+                {
+                    std::cout << "+";
+                }
+                else if(i == 0 || i == h-1)
+                {
+                    std::cout << "-";
+                }
+                else
+                {
+                    std::cout << "|";
+                }
+            }
+            else
+            {
+                std::cout << draw;
+            }
+        }
+    }
+    std::cout << "\n";
+ }
+ return 0;
 }
