ccbd0f6d09d1dbe07614a2319d8ea8e6d93014b0
hamidreza maddah <hamidrezamaddah@yahoo.com>
2022-08-28 22:21:08 +0430

    add cosine table

--- 03-sintable/sintable.cpp
+++ 03-sintable/sintable.cpp
@@ -20,18 +20,29 @@ int main(int argc, char *argv[])
         std::cout << "Invalid arguments.";
         return 1;
     }
+    bool cosine = false;
+    if (argc == 4 && argv[3][0] == 'c')
+    {
+        cosine = true;
+    }
+
     int num_samples = atoi(argv[1]);
     int num_bits = atoi(argv[2]);
 
-    int max_value = (1 << num_bits-1) -1;
-    double offset = 360.0 / num_samples;
-    
+    int max_value = (1 << num_bits - 1) - 1;
+    double interval = 2 * M_PI / num_samples;
 
-    for(int i = 0 ; i < num_samples ; i++)
+    for (int i = 0; i < num_samples; i++)
     {
-        double current_degree = i*offset;
-        int current_value = sin(current_degree*M_PI/180)*max_value;
-        std::cout << current_value << '\n';
+        double current_degree = i * interval;
+        int current_value = sin(current_degree) * max_value;
+        std::cout << current_value;
+        if (cosine)
+        {
+            current_value = cos(current_degree) * max_value;
+            std::cout <<'\t' << current_value;
+        }
+        std::cout << '\n';
     }
 
     return 0;
