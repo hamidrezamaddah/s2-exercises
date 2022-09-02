6bf0f4df7745fda4a3a3ecaa8d28cbbe15bb1a34
hamidreza maddah <hamidrezamaddah@yahoo.com>
2022-09-02 13:46:14 +0430

    release

--- 04-class_grades/classgrades.cpp
+++ 04-class_grades/classgrades.cpp
@@ -5,14 +5,14 @@
 #include <numeric>
 #include <optional>
 #include <sstream>
-#include <string>
 #include <utility>
 #include <vector>
 
 using grade_t = std::pair<std::string, double>;
 using grades_t = std::vector<grade_t>;
 
-void display_operations() {
+void display_operations()
+{
   std::cout << "\
 #####################################\n\
 #      Class Grades Management      #\n\
@@ -27,21 +27,25 @@ void display_operations() {
 select command>";
 }
 
-std::optional<grades_t> analize_file(const std::string &path) {
+std::optional<grades_t> analize_file(const std::string &path)
+{
   std::ifstream grade_file;
   std::string line;
   grade_file.open(path);
-  if (!grade_file) {
+  if (!grade_file)
+  {
     return std::nullopt;
   }
   grades_t grades;
-  while (std::getline(grade_file, line)) {
+  while (std::getline(grade_file, line))
+  {
     std::istringstream iss(line);
     double grade;
     std::string name;
     iss >> grade;
     iss >> name;
-    if (name.empty()) {
+    if (name.empty())
+    {
       continue;
     }
     grades.push_back({name, grade});
@@ -49,59 +53,59 @@ std::optional<grades_t> analize_file(const std::string &path) {
   return grades;
 }
 
-void display_all_grades(grades_t &grades) {
-  grades_t::iterator it;
+void display_all_grades(const grades_t &grades)
+{
   std::cout << "Class Grades:\n";
-  for (it = grades.begin(); it != grades.end(); it++) {
-    std::cout << it->second << '\t' << it->first << '\n';
+  for (auto &grade : grades)
+  {
+    std::cout << grade.second << '\t' << grade.first << '\n';
   }
 }
 
-void sort_grades(grades_t &grades) {
-  std::sort(grades.begin(), grades.end(),
-            [](const grade_t &l, const grade_t &r) {
-              if (l.second != r.second) {
-                return l.second < r.second;
-              }
-              return l.first < r.first;
-            });
-}
-
-void display_best_grade(grades_t &grades) {
-  sort_grades(grades);
-  grades_t::iterator best = grades.end() - 1;
+void display_best_grade(const grades_t &grades)
+{
+  auto best = std::max_element(grades.begin(), grades.end(), [](const grade_t &l, const grade_t &r)
+                               { return l.second < r.second; });
   std::cout << "The Best Grade is " << best->second << " from " << best->first
             << '\n';
 }
 
-double get_average(grades_t &grades) {
+auto get_average(const grades_t &grades)
+{
   auto sum{0.0};
-  for (auto grade : grades) {
+  for (auto &grade : grades)
+  {
     sum += grade.second;
   }
   return sum / grades.size();
 }
 
-void display_average(grades_t &grades) {
+void display_average(const grades_t &grades)
+{
   std::cout << "Class average grade is " << get_average(grades) << '\n';
 }
 
-void display_failed_grades(grades_t &grades) {
-  grades_t::iterator it;
+void display_failed_grades(const grades_t &grades)
+{
   std::cout << "Failed Students:\n";
-  for (it = grades.begin(); it != grades.end(); it++) {
-    if (it->second < 10) {
-      std::cout << it->second << '\t' << it->first << '\n';
+  for (auto &grade : grades)
+  {
+    if (grade.second < 10)
+    {
+      std::cout << grade.second << '\t' << grade.first << '\n';
     }
   }
 }
 
-void run_commands(grades_t &grades) {
-  while (true) {
+void run_commands(const grades_t &grades)
+{
+  while (true)
+  {
     display_operations();
     int cmd;
     std::cin >> cmd;
-    switch (cmd) {
+    switch (cmd)
+    {
     case 1:
       display_all_grades(grades);
       break;
@@ -123,16 +127,18 @@ void run_commands(grades_t &grades) {
   }
 }
 
-int main(int argc, char **argv) {
-
-  std::string path = (argc == 2) ? argv[1] : "grades.txt";
+int main(int argc, char **argv)
+{
+  auto path = (argc == 2) ? argv[1] : "grades.txt";
   auto result = analize_file(path);
-  if (!result) {
+  if (!result)
+  {
     std::cout << "File is not exist!\n";
     return 1;
   }
   auto &grades = result.value();
-  if (grades.size() == 0) {
+  if (grades.size() == 0)
+  {
     std::cout << "File is empty!\n";
     return 0;
   }
