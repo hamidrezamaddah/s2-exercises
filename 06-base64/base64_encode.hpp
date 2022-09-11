413b2ab2a8029e462c6d934dbb3758732183d548
hamidreza maddah <hamidrezamaddah@yahoo.com>
2022-09-11 21:16:27 +0430

    modify base64encode

--- 06-base64/base64_encode.hpp
+++ 06-base64/base64_encode.hpp
@@ -11,6 +11,7 @@ class base64_encode_t
 private:
     std::array<char, 64> base64_alphabet;
     std::vector<uint8_t> input_data;
+    static constexpr unsigned int base64_char_num_bit = 6;
 
     constexpr void fill_base64_alphabet_table()
     {
@@ -40,20 +41,20 @@ private:
         data &= ~(1 << bit);
     } 
 
-    bool get_bit(uint8_t *data, uint16_t bitoffset)
+    bool get_bit(uint8_t *data, unsigned int bitoffset)
     {
         return ((1 << ((7 - bitoffset) % 8)) & data[bitoffset / 8]) ? 1 : 0;
     }
 
-    uint8_t bitcpy(uint8_t *source, uint16_t sourceOffset, uint16_t noBits)
+    uint8_t get_char(uint8_t *source, unsigned int sourceOffset)
     {
         uint8_t dest = 0;
-        for (uint16_t i = 0; i < noBits; i++)
+        for (unsigned int i = 0; i < base64_char_num_bit; i++)
         {
             if (get_bit(source, i + sourceOffset))
-                sbi(dest, 5 - (i % 8));
+                sbi(dest, (base64_char_num_bit - 1) - i);
             else
-                cbi(dest, 5 - (i % 8));
+                cbi(dest, (base64_char_num_bit - 1) - i);
         }
         return dest;
     }
@@ -63,7 +64,7 @@ private:
         unsigned int num_bytes = input_data.size();
         for (unsigned int i = num_bytes, j = 0;; i++, j++)
         {
-            if (((i * 8) % 6) == 0)
+            if (((i * 8) % base64_char_num_bit) == 0)
             {
                 return j;
             }
@@ -93,13 +94,18 @@ public:
         }
     }
 
+    unsigned int round_up_div(unsigned int num, unsigned int div_to)
+    {
+        return (num + div_to - 1) / div_to;
+    }
+
     std::string convert()
     {
         std::string encoded;
-        unsigned int num_encoded_char = std::ceil(input_data.size() * 8 / 6.0);
+        unsigned int num_encoded_char = round_up_div(input_data.size() * 8 , base64_char_num_bit);
         for (int i = 0; i < num_encoded_char; i++)
         {
-            uint8_t temp = bitcpy(input_data.data(), i * 6, 6);
+            uint8_t temp = get_char(input_data.data(), i * base64_char_num_bit);
             encoded += base64_alphabet[temp];
         }
         for (unsigned int i = 0; i < num_pads(); i++)
