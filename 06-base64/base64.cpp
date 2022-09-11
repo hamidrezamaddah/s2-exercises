73a425732274925c4b6cda7250ce3dd56f4d200d
hamidreza maddah <hamidrezamaddah@yahoo.com>
2022-09-11 12:56:59 +0430

    add cmake

--- 06-base64/base64.cpp
+++ 06-base64/base64.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
-
+#include "bit_funcs.h"
 
 int main(int argc, char* argv[])
 {
+    std::cout << "base64\r\n";
     return 0;
 }
--- 06-base64/bit_funcs.cpp
+++ 06-base64/bit_funcs.cpp
@@ -1,24 +1,25 @@
-/*
- * BitFuncs.cpp
- *
- *  Created on: Jul 5, 2020
- *      Author: maddah
- */
+#include "bit_funcs.h"
+#include <cstdint>
 
-#include <BitFuncs.h>
-#include "GlobalDef.h"
+#define readbit(byte,add) (((byte)&(add)) ? 1 : 0)
+#define SetBit(Register,Bit)		( Register|=(Bit))
+#define ClrBit(Register,Bit)		( Register&=(~(Bit)))
+#define CplBit(Register,Bit)		( Register^=(Bit))
+#define sbi(port,bit)				(port |=  (1<<(bit)))
+#define cbi(port,bit)				(port &= ~(1<<(bit)))
+#define rbi(port,bit)				(port & (1<<(bit)))
 
-BOOLEAN get_bit(UINT8 *data, UINT16 bitoffset)
+bool get_bit(uint8_t *data, uint16_t bitoffset)
 {
 	return ( (1 << (bitoffset & 7)) & data[bitoffset >> 3] ) ? 1 : 0 ; // X&7 is X%8 , X>>3 is X/8
 }
 
-UINT8 get_bits(UINT8* data, UINT16 bitOffset, UINT8 numBits)
+uint8_t get_bits(uint8_t* data, uint16_t bitOffset, uint8_t numBits)
 {
-    UINT8 bits = 0;
+    uint8_t bits = 0;
 	if ( numBits == 0 )
 		return 0;
-    for (UINT16 currentbit = bitOffset + (numBits-1) ; currentbit >= bitOffset; currentbit--)
+    for (uint16_t currentbit = bitOffset + (numBits-1) ; currentbit >= bitOffset; currentbit--)
     {
     	bits = bits << 1;
     	bits = bits | get_bit(data, currentbit);
@@ -27,9 +28,9 @@ UINT8 get_bits(UINT8* data, UINT16 bitOffset, UINT8 numBits)
     }
     return bits;
 }
-void bitcpy(UINT8* source,UINT16 sourceOffset,UINT8* dest, UINT16 destOffset, UINT16 noBits)
+void bitcpy(uint8_t* source,uint16_t sourceOffset,uint8_t* dest, uint16_t destOffset, uint16_t noBits)
 {
-	for (UINT16 i = 0 ; i < noBits ; i++)
+	for (uint16_t i = 0 ; i < noBits ; i++)
 	{
 		if ( get_bit(source, i + sourceOffset) )
 			sbi(dest[(destOffset+i)>>3],(i+destOffset)&7);
@@ -39,9 +40,9 @@ void bitcpy(UINT8* source,UINT16 sourceOffset,UINT8* dest, UINT16 destOffset, UI
 
 }
 // this function copy "noBits" bits from " LSB of source " to " bitOffset Bit of dest "
-void put_bits(UINT8 source,UINT8 noBits,UINT8 *dest,UINT16 bitOffset)
+void put_bits(uint8_t source,uint8_t noBits,uint8_t *dest,uint16_t bitOffset)
 {
-	for (UINT8 i = 0 ; i < noBits ; i++)
+	for (uint8_t i = 0 ; i < noBits ; i++)
 	{
 		if ( get_bit(&source, i) )
 			sbi(dest[(bitOffset+i)>>3],(i+bitOffset)&7);
@@ -50,9 +51,9 @@ void put_bits(UINT8 source,UINT8 noBits,UINT8 *dest,UINT16 bitOffset)
 	}
 }
 
-void bit_memset(UINT8 *array,UINT16 bitOffset,UINT16 len,BOOLEAN value)
+void bit_memset(uint8_t *array,uint16_t bitOffset,uint16_t len,bool value)
 {
-	for (UINT8 i = 0 ; i < len ; i++)
+	for (uint8_t i = 0 ; i < len ; i++)
 	{
 		if ( value )
 			sbi(array[(bitOffset+i)>>3],(i+bitOffset)&7);
@@ -61,10 +62,10 @@ void bit_memset(UINT8 *array,UINT16 bitOffset,UINT16 len,BOOLEAN value)
 	}
 }
 
-UINT16 CheckForZeroBitsInBuff(UINT8 *buff,UINT16 Bitlen)
+uint16_t CheckForZeroBitsInBuff(uint8_t *buff,uint16_t Bitlen)
 {
- UINT16 NoCmpltByte=0,i,NoZeros=0;
- UINT8 NoBitsInLastByte=0;
+ uint16_t NoCmpltByte=0,i,NoZeros=0;
+ uint8_t NoBitsInLastByte=0;
  NoCmpltByte=Bitlen/8;
  NoBitsInLastByte=Bitlen%8;
  for (i=0;i<NoCmpltByte;i++) NoZeros+=CountZeroBitsInByte(buff[i],8);
@@ -72,9 +73,9 @@ NoZeros+= CountZeroBitsInByte(buff[i],NoBitsInLastByte);
 return NoZeros;
 }
 
-UINT8 CountZeroBitsInByte(UINT8 Byte,UINT8 BitsInByte)
+uint8_t CountZeroBitsInByte(uint8_t Byte,uint8_t BitsInByte)
 {
- UINT8 NoZeros=0,i;
+ uint8_t NoZeros=0,i;
  for (i=0;i<BitsInByte;i++)
  {
     if ( ((1<<i)&Byte) == 0 ) NoZeros++;
@@ -82,10 +83,10 @@ UINT8 CountZeroBitsInByte(UINT8 Byte,UINT8 BitsInByte)
  return NoZeros;
 }
 
-UINT16 CheckForSetBitsInBuff(UINT8 *buff,UINT16 Bitlen)
+uint16_t CheckForSetBitsInBuff(uint8_t *buff,uint16_t Bitlen)
 {
- UINT16 NoCmpltByte=0,i,No=0;
- UINT8 NoBitsInLastByte=0;
+ uint16_t NoCmpltByte=0,i,No=0;
+ uint8_t NoBitsInLastByte=0;
  NoCmpltByte=Bitlen/8;
  NoBitsInLastByte=Bitlen%8;
  for (i=0;i<NoCmpltByte;i++) No +=CountSetBitsInByte(buff[i],8);
@@ -93,9 +94,9 @@ No+= CountSetBitsInByte(buff[i],NoBitsInLastByte);
 return No;
 }
 
-UINT8 CountSetBitsInByte(UINT8 Byte,UINT8 BitsInByte)
+uint8_t CountSetBitsInByte(uint8_t Byte,uint8_t BitsInByte)
 {
- UINT8 no=0,i;
+ uint8_t no=0,i;
  for (i=0;i<BitsInByte;i++)
  {
     if ( ((1<<i)&Byte) ) no++;
@@ -103,22 +104,22 @@ UINT8 CountSetBitsInByte(UINT8 Byte,UINT8 BitsInByte)
  return no;
 }
 
-BOOLEAN EventCheckBitInByte(UINT8 refByte,UINT8 refBit,UINT8 *tempByte,UINT8 tempBit,UINT8 ev4one,UINT8 ev4zero,UINT8 *EventCode)
+bool EventCheckBitInByte(uint8_t refByte,uint8_t refBit,uint8_t *tempByte,uint8_t tempBit,uint8_t ev4one,uint8_t ev4zero,uint8_t *EventCode)
 {
 	if( (readbit(refByte,refBit)) != (readbit((*tempByte),tempBit)) )
    	{
     	CplBit((*tempByte),tempBit);
         if (refByte&refBit) (*EventCode)=ev4one;
      	else (*EventCode)=ev4zero;
-   		return TRUE;
+   		return true;
    	}
-    return FALSE;
+    return false;
 }
 
-UINT8 MergeSubniblesToByte(UINT8 subnible_3, UINT8 subnible_2, UINT8 subnible_1, UINT8 subnible_0)
+uint8_t MergeSubniblesToByte(uint8_t subnible_3, uint8_t subnible_2, uint8_t subnible_1, uint8_t subnible_0)
 {
    // declare local variables
-   UINT8 data;
+   uint8_t data;
 
    // get subnible_3
    data = subnible_3;
@@ -138,10 +139,10 @@ UINT8 MergeSubniblesToByte(UINT8 subnible_3, UINT8 subnible_2, UINT8 subnible_1,
    // return byte
    return data;
 }
-UINT16 MergeBytesToWord(UINT8 highByte, UINT8 lowByte)
+uint16_t MergeBytesToWord(uint8_t highByte, uint8_t lowByte)
 {
    // declare local variables
-   UINT16 data;
+   uint16_t data;
 
    // get high byte
    data = highByte;
